3df3db52ff95a836ecdb00260fa17e7908a7c241
Polina Tutygina <polina.tutygina@yandex.ru>
2024-10-21 16:49:53 +0300

    Обновлены файлы: base.html, header.html и footer.html

--- anfisa_for_friends/anfisa_for_friends/settings.py
+++ anfisa_for_friends/anfisa_for_friends/settings.py
@@ -90,7 +90,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static_dev',
+    'D:/Dev/anfisa1sprint/html_templates',
     ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
